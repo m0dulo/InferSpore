0bbabeac1af504f9769ad3f509af6f2916e0314c
m0dulo <m0dulo.lyx@gmail.com>
2025-04-03 18:31:43 +0800

    feat: implement layer weights

--- src/weights/llama/layer_weights.cc
+++ src/weights/llama/layer_weights.cc
@@ -77,7 +77,111 @@ void LlamaLayerWeight<T>::loadWeights(std::string weight_path, WeightType weight
     //ffn_weight.down.bias = (T*)d_dummy_ffn_down_bias;
 }
 
+// (m0dulo)note: load dummy model/weight API, is used to the time when you want test inference performance only
+template<typename T>
+void LlamaLayerWeight<T>::loadWeights() 
+{
+    T* d_dummy_attn_norm_weight;
+    T* d_dummy_ffn_norm_weight;
+    T* d_dummy_qkv_weights;
+    //T* d_dummy_qkv_bias;
+    T* d_dummy_output_weights;
+    T* d_dummy_output_bias;
+    T* d_dummy_ffn_down;
+    T* d_dummy_ffn_down_bias;
+    T* d_dummy_ffn_gate_up;
+    // T* d_dummy_ffn_up;
+    CHECK(cudaMalloc((void**)&d_dummy_attn_norm_weight, sizeof(T) * hidden_units));
+    CHECK(cudaMalloc((void**)&d_dummy_ffn_norm_weight, sizeof(T) * hidden_units));
+    CHECK(cudaMalloc((void**)&d_dummy_qkv_weights, sizeof(T) * hidden_units * (head_num + 2 * kv_head_num) * head_size));
+   // CHECK(cudaMalloc((void**)&d_dummy_qkv_bias, sizeof(T) * (head_num + 2 * kv_head_num) * head_size));
+    CHECK(cudaMalloc((void**)&d_dummy_output_weights, sizeof(T) * hidden_units * hidden_units));
+    CHECK(cudaMalloc((void**)&d_dummy_output_bias, sizeof(T) * hidden_units));
+    CHECK(cudaMalloc((void**)&d_dummy_ffn_down, sizeof(T) * hidden_units * inter_size));
+    CHECK(cudaMalloc((void**)&d_dummy_ffn_down_bias, sizeof(T) * hidden_units));
+    CHECK(cudaMalloc((void**)&d_dummy_ffn_gate_up, sizeof(T) * hidden_units * 2 * inter_size));
+    // CHECK(cudaMalloc(&d_dummy_ffn_up, sizeof(T) * hidden_units * inter_size));
 
+    T* h_dummy_attn_norm_weight = (T*)malloc(sizeof(T) * hidden_units);
+    T* h_dummy_ffn_norm_weight = (T*)malloc(sizeof(T) * hidden_units);
+    T* h_dummy_qkv_weights = (T*)malloc(sizeof(T) * hidden_units * (head_num + 2 * kv_head_num) * head_size);
+   // T* h_dummy_qkv_bias = (T*)malloc(sizeof(T) * (head_num + 2 * kv_head_num) * head_size);
+    T* h_dummy_output_weights = (T*)malloc(sizeof(T) * hidden_units * hidden_units);
+    T* h_dummy_output_bias = (T*)malloc(sizeof(T) * hidden_units);
+    T* h_dummy_ffn_down = (T*)malloc(sizeof(T) * hidden_units * inter_size);
+    T* h_dummy_ffn_down_bias = (T*)malloc(sizeof(T) * hidden_units);
+    T* h_dummy_ffn_gate_up = (T*)malloc(sizeof(T) * hidden_units * 2 * inter_size);
+    // T* h_dummy_ffn_up = (T*)malloc(sizeof(T) * hidden_units * inter_size);
 
+    for (int i = 0; i < hidden_units; i++){
+        h_dummy_attn_norm_weight[i] = (T)(rand() % 100 / (float)100000);
+        h_dummy_ffn_norm_weight[i] = (T)(rand() % 100 / (float)100000);
+        h_dummy_output_bias[i] = (T)(rand() % 100 / (float)100000);
+        h_dummy_ffn_down_bias[i] = (T)(rand() % 100 / (float)100000);
+    }
+    //for (int i = 0; i < (head_num + 2 * kv_head_num) * head_size; i++) {
+    //    h_dummy_qkv_bias[i] = (T)(rand() % 100 / (float)100000);
+    //}
+    for (int i = 0; i < hidden_units * inter_size; i++) {
+        h_dummy_ffn_down[i] = (T)(rand() % 100 / (float)100000);
+    }
+    for (int i = 0; i < hidden_units * 2 * inter_size; i++) {   
+        h_dummy_ffn_gate_up[i] = (T)(rand() % 100 / (float)100000);
+        // h_dummy_ffn_up[i] = (T)1.0f;
+    }
+    for (int i = 0; i < hidden_units * hidden_units; i++) {
+        h_dummy_output_weights[i] = (T)(rand() % 100 / (float)100000);
+    }
+    for (int i = 0; i < hidden_units * (head_num + 2 * kv_head_num) * head_size; i++) {
+        h_dummy_qkv_weights[i] = (T)(rand() % 100 / (float)100000);
+    }
+    CHECK(cudaMemcpy(d_dummy_attn_norm_weight, h_dummy_attn_norm_weight, sizeof(T) * hidden_units, cudaMemcpyHostToDevice));
+    CHECK(cudaMemcpy(d_dummy_ffn_norm_weight, h_dummy_ffn_norm_weight, sizeof(T) * hidden_units, cudaMemcpyHostToDevice));
+    CHECK(cudaMemcpy(d_dummy_qkv_weights, h_dummy_qkv_weights, sizeof(T) * hidden_units * (head_num + 2 * kv_head_num) * head_size, cudaMemcpyHostToDevice));
+    //CHECK(cudaMemcpy(d_dummy_qkv_bias, h_dummy_qkv_bias, sizeof(T) * (head_num + 2 * kv_head_num) * head_size, cudaMemcpyHostToDevice));
+    CHECK(cudaMemcpy(d_dummy_output_weights, h_dummy_output_weights, sizeof(T) * hidden_units * hidden_units, cudaMemcpyHostToDevice));
+    CHECK(cudaMemcpy(d_dummy_output_bias, h_dummy_output_bias, sizeof(T) * hidden_units, cudaMemcpyHostToDevice));
+    CHECK(cudaMemcpy(d_dummy_ffn_down, h_dummy_ffn_down, sizeof(T) * hidden_units * inter_size, cudaMemcpyHostToDevice));
+    CHECK(cudaMemcpy(d_dummy_ffn_down_bias, h_dummy_ffn_down_bias, sizeof(T) * hidden_units, cudaMemcpyHostToDevice));
+    CHECK(cudaMemcpy(d_dummy_ffn_gate_up, h_dummy_ffn_gate_up, sizeof(T) * hidden_units * 2 * inter_size, cudaMemcpyHostToDevice));
+    // CHECK(cudaMemcpy(d_dummy_ffn_up, h_dummy_ffn_up, sizeof(T) * hidden_units * inter_size, cudaMemcpyHostToDevice));
+    // before kernel launch, the ptr is always void*, when luanching kernel, ptr type will be cast to float* or T*
+    attn_norm_weight.gamma = d_dummy_attn_norm_weight;
+    ffn_norm_weight.gamma = d_dummy_ffn_norm_weight;
+    self_attn_weight.qkv.data = d_dummy_qkv_weights;
+    self_attn_weight.qkv.bias = nullptr;
+    self_attn_weight.output.data = d_dummy_output_weights;
+    self_attn_weight.output.bias = d_dummy_output_bias;
+    ffn_weight.gateAndup.data = d_dummy_ffn_gate_up;
+    //ffn_weight.up.data = d_dummy_ffn_up;
+    ffn_weight.down.data = d_dummy_ffn_down;
+    ffn_weight.down.bias = d_dummy_ffn_down_bias;
+}
+
+template<typename T>
+void freeWeights(BaseWeight<T>& weights)
+{
+    cudaFree(weights.data);
+    if(weights.bias != nullptr) {
+        cudaFree(weights.bias);
+    }
+
+    weights.data = nullptr;
+    weights.bias = nullptr;
+}
+template<typename T>
+LlamaLayerWeight<T>::~LlamaLayerWeight()
+{
+    // free norm weights ptr
+    cudaFree(attn_norm_weight.gamma);
+    cudaFree(ffn_norm_weight.gamma);
+    // free other weights, including data and bias
+    freeWeights(self_attn_weight.qkv);
+    freeWeights(self_attn_weight.output);
+    freeWeights(ffn_weight.gateAndup);
+    // freeWeights(ffn_weight.up);
+    freeWeights(ffn_weight.down);
+}
+// template instantial required in linking time
 template class LlamaLayerWeight<float>;
 template class LlamaLayerWeight<half>;
